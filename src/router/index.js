9cb83f7eeffdec9309421b051db0a5dba5b61271
Dimitar Dimitrov <dimitardddm97@gmail.com>
2021-01-18 18:10:46 +0200

    added profile simple route

--- src/router/index.js
+++ src/router/index.js
@@ -18,6 +18,12 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/profile",
+    name: "Profile",
+    component: () =>
+      import(/* webpackChunkName: "profile" */ "../views/Profile.vue")
   }
 ];
 
