117791d45d582ab135ad063c95cba04682624a96
Dimitar Dimitrov <dimitardddm97@gmail.com>
2021-01-19 16:51:29 +0200

    exercising watchers

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -6,6 +6,12 @@
         <router-link :to="{ name: 'Info', params: { id:'4321' } }"> Profile Info</router-link>
         <br>
         <router-link :to="{ name: 'ProfileWithId', params: { id:'4321' }, query: { profile: 'private'} }"> Profile with query params</router-link>
+        <br>
+        <h1>{{fullname}}</h1>
+        <input type="text" v-model.lazy="user.firstName">
+        <h1>{{user.firstName}}</h1>
+        <input type="text" v-model.lazy="firstNameUser">
+        <h1>{{firstNameUser}}</h1>
     <router-view></router-view>
     </div>
 </template>
@@ -13,14 +19,37 @@
 <script>
     export default {
          name: "Profile",
-        //  mounted(){
-        //      console.log(this.$route.params.id)
-        //  },
-         watch:{
+         data()
+         {
+             return {
+                user: {
+                    firstName: 'Peter',
+                    fathersName: 'Angleov',
+                    surname:'Damqnov'
+                },
+             }
+         },
+         mounted(){
+             console.log(this.$route.params.id)
+         },
+         computed:{ //the computed property automatically updates when the propertyes it deppends on are changed
+             fullname: function() {
+                 return [this.user.firstName,this.user.fathersName,this.user.surname].join(' ');
+             }
+         },
+
+         watch:{ //watchers are called observables 
              $route(to,from){
                  console.log('to:', to)
                  console.log('from:', from)
-             }
+             },
+             fullname(){
+                 alert('user details have been changed')
+             },
+             'user.firstName': function(newVal, oldVal){
+                //to work with changes in prop
+                console.log(`the new value is ${newVal} and the old one is ${oldVal}`)
+            }
 
          }
     }
