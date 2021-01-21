dcb51835a830de95d5ba61c5371cf427e3de8dbe
Dimitar Dimitrov <dimitardddm97@gmail.com>
2021-01-21 17:38:13 +0200

    Added search and recipes component

---
+++ src/components/Recipes.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="recipe-name">{{ recipe.title }}</div>
+    <img class="recipe-img" :src="recipe.image" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Recipes",
+  props: {
+    recipe: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
+
+<style scoped></style>
---
+++ src/views/Search.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <input type="text" />
+    <ul v-for="recipe in recipes" :key="recipe.id">
+      <recipes :recipe="recipe"></recipes>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Recipes from "@/components/Recipes";
+export default {
+  name: "Search",
+  components: {
+    Recipes
+  },
+  computed:{
+      
+  }
+};
+</script>
+
+<style scoped></style>
