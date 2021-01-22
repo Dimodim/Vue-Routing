4288793dc16566a9730bd60d6f93c5ffa206fd81
Dimitar Dimitrov <dimitardddm97@gmail.com>
2021-01-22 16:28:17 +0200

    added firebase to proj

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
       <router-link :to="{ name: 'Profile' }">Profile</router-link> |
-      <router-link :to="{ name: 'Search' }">Search</router-link>
+      <router-link :to="{ name: 'Search' }">Search</router-link> |
+      <router-link :to="{ name: 'Login' }">Login</router-link> |
+      <router-link :to="{ name: 'Register' }">Register</router-link> |
+      <router-link :to="{ name: 'Secret' }">Secret</router-link> |
     </div>
     <router-view />
   </div>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,23 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import axios from "axios";
+import firebase from "firebase/app";
 
 Vue.config.productionTip = false;
+Vue.prototype.$axios = axios;
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyA1cEdBc3TL4fFb27b3R7gJXPsdwDXV7p0",
+  authDomain: "vue-firebase-auth-124e7.firebaseapp.com",
+  projectId: "vue-firebase-auth-124e7",
+  storageBucket: "vue-firebase-auth-124e7.appspot.com",
+  messagingSenderId: "714271383309",
+  appId: "1:714271383309:web:804cb9bd87cc5551e6da44"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
