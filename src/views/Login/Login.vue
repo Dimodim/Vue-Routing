4359314e616810234fc2470d7d55039287893c76
Dimitar Dimitrov <dimitardddm97@gmail.com>
2021-01-22 16:28:55 +0200

    added compopnents

---
+++ src/views/Login/Login.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>Login</h1>
+    <div>
+      <input type="text" />
+      <input type="password" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login"
+};
+</script>
+
+<style scoped>
+.error {
+  color: red;
+}
+
+input {
+  width: 400x;
+  padding: 30px;
+  margin: 20px;
+  font-size: 21px;
+}
+
+button {
+  width: 400px;
+  height: 75px;
+  font-size: 100%;
+}
+</style>
---
+++ src/views/Login/Secret.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Secret</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Secret"
+};
+</script>
+
+<style scoped></style>
